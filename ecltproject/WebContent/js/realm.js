f824d08582ee5da4f112043f10cd34794906cbcf
1778650591 <1778650591@qq.com>
2019-10-11 15:15:41 +0800

    前端代码优化

--- ecltproject/WebContent/js/realm.js
+++ ecltproject/WebContent/js/realm.js
@@ -2,3 +2,20 @@
 //全局域名
 const serverSrc = "http://localhost:8080/ecltproject/";
 
+//产品默认图片
+	//--详情小图
+const productMinimg = "images/felimgbar.png";
+	//--轮播图
+const productImg = "images/chanpxq.png";
+
+//案例默认图片
+	//--详情小图
+const caseImg = "images/Usmainpages.jpg";
+	//--详情大图
+const caseMaximg = "images/Usmainpages.jpg";
+
+//认证默认图片
+	//--详情小图
+const ncpImg = "images/zhuanli.jpg";
+	//--详情大图
+const ncpMaximg = "images/zhuanli.jpg";
